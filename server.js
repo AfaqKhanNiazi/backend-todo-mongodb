73ddd5714ace66a3fe1892c51156ed8c1fb88211
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-01-20 18:08:57 +0500

    try catch

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import 'dotenv/config'
 import './database.js';
@@ -6,14 +6,14 @@ import { Todo } from "./models/index.js";
 
 
 const app = express();
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5000;
 
 // to convert body into json
 app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173","https://back-end-todo-list.surge.sh","https://todo-mongodb-sand.vercel.app",
+      "http://localhost:5173","https://back-end-todo-list.surge.sh",
     ],
   }),
 );
@@ -21,67 +21,80 @@ app.use(
 app.get("/api/v1/todos", async(request, response) => {
 
  try {
-  const todos = await Todo.find({},
-    {ip:0, __v:0, updatedAt: 0 });
+  const todos = await Todo.find(
+    {},
+    // {ip:0, __v:0, updatedAt: 0 });
+    {todoContent:1}
+  );
   
     const message = !todos.length ? "todos empty" : "ye lo sub todos";
-    response.send({ data: todos, message: message });
- } catch (err) {
+    response.status(200).send({ data: todos, message: message });
+ } catch (error) {
   response.status(500).send("internal server error");
  }
 });
 
 
-app.post("/api/v1/todo", async (request, response) => {
-  const obj = {
-    todoContent: request.body.todo,
-    ip: request.ip,
-  };
+// app.post("/api/v1/todo", async (request, response) => {
+//   const obj = {
+//     todoContent: request.body.todo,
+//     ip: request.ip,
+//   };
 
-  const result = await Todo.create(obj)
+//   const result = await Todo.create(obj)
 
-  response.send({ message: "todo add hy", data: result });
-});
-
-// app.post("/api/v1/todo", async(request, response) => {
-//   try {
-//     const obj = {
-//       todoContent: request.body.todo,
-//       ip: request.ip
-//     };
-  
-//     const result = await Todo.create(obj)
-    
-//     response.send({ message: "todo add ho gaya ha", data:result });
-//   } catch (error) {
-//     response.status(501).send("error ha koi tw")
-//   }
-  
-  
+//   response.send({ message: "todo add hy", data: result });
 // });
 
 
-app.patch("/api/v1/todo/:id", async(request, response) => {
-  const id = request.params.id;
-
- const result = await Todo.findByIdAndUpdate(id,
-  {todoContent: request.body.todoContent}
-)
-console.log('result=>',result);
-
-
-  if (result) {
-    response.status(201).send({
-      data: result,
-      message: "todo updated successfully!",
-    });
-  } else {
-    response.status(200).send({ data: null, message: "todo not found" });
+app.post("/api/v1/todo", async(request, response) => {
+  try {
+    const { todo } = request.body.todo;
+    const obj = {
+      todoContent: request.body.todo,
+      ip: request.ip
+    };
+  
+    const result = await Todo.create(obj)
+    
+    response.status(201).send({data:result, message: "todo add ho gaya ha"});
+  } catch (error) {
+    response.status(501).send("error ha koi tw")
   }
+  
 });
 
-app.delete("/api/v1/todo/:id", async(request, response) => {
+
+app.patch("/api/v1/todo/:id", async(request, response) => {
+
+try {
   const id = request.params.id;
+
+  const result = await Todo.findByIdAndUpdate(id,
+   {todoContent: request.body.todoContent}
+ )
+ console.log('result=>',result);
+ 
+ 
+   if (result) {
+     response.status(201).send({
+       data: result,
+       message: "todo updated successfully!",
+     });
+   } else {
+     response.status(404).send({ data: null, message: "todo not found" });
+   }
+ } catch (error) {
+  response.status(404).send("id not found");
+ }
+}),
+
+
+
+app.delete("/api/v1/todo/:id", async(request, response) => {
+
+  try {
+    const id = request.params.id;
 
  const result = await Todo.findByIdAndDelete(id)
 
@@ -91,10 +104,13 @@ app.delete("/api/v1/todo/:id", async(request, response) => {
       message: "todo deleted successfully!",
     });
   } else {
-    response.status(200).send({ data: null, message: "todo not found" });
+    response.status(204).send({ data: null, message: "todo not found" });
+  }
+
+  } catch (error) {
+    response.status(204).send("No content found");
   }
 });
-
 //
 
 app.use((request, response) => {
