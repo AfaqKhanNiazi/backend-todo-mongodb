d12ca435511003546c49cd60e85517079db1d1cc
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-01-21 20:29:53 +0500

    port change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { Todo } from "./models/index.js";
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 // to convert body into json
 app.use(express.json());
